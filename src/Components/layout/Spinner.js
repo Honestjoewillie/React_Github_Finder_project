57a16d54ce08034a9f576cd924148e896ee86111
Joseph William Hauger <joseph>
2019-08-15 19:16:19 -0500

    almost done with proj 1

--- src/Components/layout/Spinner.js
+++ src/Components/layout/Spinner.js
@@ -1,8 +1,14 @@
-import React from "react";
-import spinner from "./spinner.";
+import React, { Fragment } from "react";
+import spinner from "../layout/spinner.gif";
 
-const Spinner = () => {
-  return <div />;
-};
+const Spinner = () => (
+  <Fragment>
+    <img
+      src="{spinner}"
+      alt="Loading..."
+      style={{ width: "200px", margin: "auto", display: "block" }}
+    />
+  </Fragment>
+);
 
 export default Spinner;
